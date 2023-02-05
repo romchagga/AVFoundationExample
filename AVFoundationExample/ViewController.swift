a1db5164568fd5bbe06e2b24309beee81fa3ad2d
Ramis Askerov <askerov.ramis1@gmail.com>
2023-02-05 17:21:30 +0300

    Prepare

--- AVFoundationExample/ViewController.swift
+++ AVFoundationExample/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isPlaying = false
+    
+    var playButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "play.circle"), for: .normal)
+        button.addTarget(self, action: #selector(playButtonTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setConstrainst()
+    }
+    
+    @objc func playButtonTapped() {
+       
+        if isPlaying {
+            playButton.setImage(UIImage(systemName: "play.circle"), for: .normal)
+            isPlaying = false
+        }  else {
+            playButton.setImage(UIImage(systemName: "stop.circle"), for: .normal)
+            isPlaying = true
+        }
     }
 
-
+    func setConstrainst() {
+        view.addSubview(playButton)
+        
+        NSLayoutConstraint.activate([
+            playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            playButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
 }
 
